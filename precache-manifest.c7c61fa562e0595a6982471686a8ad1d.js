6c3eb1fe976252d7ca80581ace3a7cb1f55d3287
thearturord <47672781+thearturord@users.noreply.github.com>
2024-05-25 02:32:45 +0200

    Add files via upload

---
+++ precache-manifest.c7c61fa562e0595a6982471686a8ad1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a14c3db96aa70debc6170c67607299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45c00a686596031cb67d",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "381f241008b209cc649b",
+    "url": "/static/js/2.df45cccc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.df45cccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c00a686596031cb67d",
+    "url": "/static/js/main.af1fe6b7.chunk.js"
+  },
+  {
+    "revision": "30967c40040320d0b91a",
+    "url": "/static/js/runtime-main.021e82a6.js"
+  }
+]);
